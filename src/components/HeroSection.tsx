4df7110711879316514b8832f01424360bee3dc2
Jeffrin M <mjeffrin040@gmail.com>
2025-07-31 15:49:52 +0530

    Latest fixes

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -20,7 +20,7 @@ export default function HeroSection() {
         playsInline
         className="absolute top-0 left-0 w-full h-full object-cover z-0"
       >
-        <source src="/back.mp4" type="video/mp4" />
+        <source src="/hero.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
